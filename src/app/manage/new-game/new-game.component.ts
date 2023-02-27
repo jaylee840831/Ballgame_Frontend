ea56cc5704a8a362cfdf297f7299c737b884e6d1
jasper0831 <lyinjue456852@gmail.com>
2023-02-28 02:37:57 +0800

    新增球局功能畫面開發

--- src/app/manage/new-game/new-game.component.ts
+++ src/app/manage/new-game/new-game.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-new-game',
   templateUrl: './new-game.component.html',
-  styleUrls: ['./new-game.component.scss']
+  styleUrls: ['./new-game.component.css']
 })
 export class NewGameComponent {
 
